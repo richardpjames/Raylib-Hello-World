a704c879676305f203ffd9974d07c26de3dff9df
Richard James <richard@richardpjames.com>
2024-10-19 12:53:59 +0100

    Initial changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+namespace HelloWorld;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(1920, 1080, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 64, Color.DarkGray);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Read Me
+
+This is just a skeleton project to prove the installation of RayLib and the C# bindings.
+
+This is as simple as installing the Raylib-CS NuGet package and then running the code in this repository.
